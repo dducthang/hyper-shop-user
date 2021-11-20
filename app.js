59787a89c80afbc575aa7a71483170f79a497647
Nam <nguyenthenam1910@gmail.com>
2021-11-20 20:08:03 +0700

    test

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-require('./db/mongoose.js');
-require('dotenv/config');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+require("./db/mongoose.js");
+require("dotenv/config");
 
-const shopRouter = require('./routers/shop');
+const shopRouter = require("./routers/shop");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('tiny'));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("tiny"));
 
 app.use(shopRouter);
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Server is running on http://localhost:3000');
-})
-
+//test change
