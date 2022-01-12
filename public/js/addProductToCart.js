8feac2e1002a2e3b567dfc6676d212fdcee0410d
Tung Nguyen <ncnt257@gmail.com>
2022-01-12 14:02:29 +0700

    fix bug: absolute url in cart module

--- public/js/addProductToCart.js
+++ public/js/addProductToCart.js
@@ -1,31 +1,33 @@
-const addToCartHandler =(event)=>{
-    fetch('http://localhost:3000/cart/add', {
-        method: 'post',
-        body: JSON.stringify({
-            id:event.target.id
-        }),
-        headers:{
-            'Content-type':'application/json'
-        }
-    }).then(response=>{
-        if (response.status>= 200 && response.status<300){
-            alert('Add product seccessfully');
-            return response.json();
-        }else{
-            response.json().then(error=>{
-                console.log('ERROR: '+error);
-            });
-        }
-    }).catch(error=>{
-        console.log(error);
+const addToCartHandler = event => {
+  fetch('/cart/add', {
+    method: 'post',
+    body: JSON.stringify({
+      id: event.target.id,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+        alert('Add product seccessfully');
+        return response.json();
+      } else {
+        response.json().then(error => {
+          console.log('ERROR: ' + error);
+        });
+      }
     })
-}
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 /*
 <script id="scriptAddProductToCart" type="text/javascript">
     const addToCartHandler =(event)=>{
         console.log(event.toElement)
-        fetch('http://localhost:3000/cart/add', {
+        fetch('/cart/add', {
             method: 'post',
             body: JSON.stringify({
                 id:event.target.id
